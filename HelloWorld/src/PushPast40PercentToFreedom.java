214d3de35ce88ff0758dd9a3dc830e734624463a
jmintegr1 <jmintegr1@gmail.com>
2018-12-18 21:54:47 -0500

    Life & World is Suffering

---
+++ HelloWorld/src/PushPast40PercentToFreedom.java
@@ -0,0 +1,9 @@
+
+public class PushPast40PercentToFreedom {
+
+	public static void main(String[] args) {
+		System.out.println("I love purposeful pain, I run to ligitimate pain as I push past it I get stronger and grow to be a "
+				+ " better coder!!!!");
+	}
+
+}
